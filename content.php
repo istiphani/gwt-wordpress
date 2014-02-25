c8d9c9abe18a6ae228f560820cc63bb6998e41ad
istiphani <ssazarias@gmail.com>
2014-02-25 14:12:37 +0800

    WordPress template for iGov websites ONLY

--- content.php
+++ content.php
@@ -11,7 +11,7 @@
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php gwt_wp_posted_on(); ?>
+			<!--?php gwt_wp_posted_on(); ?-->
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -32,34 +32,34 @@
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
+	<!--footer class="entry-meta">
+		<!--?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+			<!--?php
 				/* translators: used between list items, there is a space after the comma */
 				$categories_list = get_the_category_list( __( ', ', 'gwt_wp' ) );
 				if ( $categories_list && gwt_wp_categorized_blog() ) :
 			?>
 			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'gwt_wp' ), $categories_list ); ?>
+				<!--?php printf( __( 'Posted in %1$s', 'gwt_wp' ), $categories_list ); ?>
 			</span>
-			<?php endif; // End if categories ?>
+			<!--?php endif; // End if categories ?>
 
-			<?php
+			<!--?php
 				/* translators: used between list items, there is a space after the comma */
 				$tags_list = get_the_tag_list( '', __( ', ', 'gwt_wp' ) );
 				if ( $tags_list ) :
 			?>
 			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'gwt_wp' ), $tags_list ); ?>
+				<!--?php printf( __( 'Tagged %1$s', 'gwt_wp' ), $tags_list ); ?>
 			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
+			<!--?php endif; // End if $tags_list ?>
+		<!--?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'gwt_wp' ), __( '1 Comment', 'gwt_wp' ), __( '% Comments', 'gwt_wp' ) ); ?></span>
-		<?php endif; ?>
+		<!--?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		<span class="comments-link"><!--?php comments_popup_link( __( 'Leave a comment', 'gwt_wp' ), __( '1 Comment', 'gwt_wp' ), __( '% Comments', 'gwt_wp' ) ); ?></span>
+		<!--?php endif; ?>
 
-		<?php edit_post_link( __( 'Edit', 'gwt_wp' ), '<span class="edit-link">', '</span>' ); ?>
+		<!--?php edit_post_link( __( 'Edit', 'gwt_wp' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
 </div><!-- .post-box -->
