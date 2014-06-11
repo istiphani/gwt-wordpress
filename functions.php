ba3dbdfb87bbf71781a8821ec879f8b632f01ea5
gracenaagas <gracenaagas@users.noreply.github.com>
2014-06-10 22:06:49 -0400

    Updated functions.php
    Added a function that will display a widget (either left or right)for Philippine Standard Time

--- functions.php
+++ functions.php
@@ -266,3 +266,86 @@ require get_template_directory() . '/inc/options.php';
  * Author URI: http://www.casabona.org
  */
 require get_template_directory() . '/inc/vendors/envato-flex-slider/envato-flex-slider.php';
+
+
+
+
+
+
+
+/**
+ * Custom Widget: PST Widget
+ * Widget URI:
+ * Description: Widget that displays PST
+ * Author: Mary Grace Fernandez-Naagas
+ * Version: 0.5
+ * Author URI: 
+ */
+ 
+// Creating the widget 
+class wpb_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct(
+// Base ID of your widget
+'wpb_widget', 
+
+// Widget name will appear in UI
+__('Philippine Standard Time', 'wpb_widget_domain'), 
+
+// Widget description
+array( 'description' => __( 'Displays Philippine Standard Time', 'wpb_widget_domain' ), ) 
+);
+}
+
+// Creating widget front-end
+// This is where the action happens
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+// before and after widget arguments are defined by themes
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+
+// This is where you run the code and display the output
+echo __( '<iframe src = "http://kidlat.pagasa.dost.gov.ph/ourtime.shtml" width="300" height="100" scrolling="no"></iframe>', 'wpb_widget_domain' );
+echo $args['after_widget'];
+}
+		
+// Widget Backend 
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'New title', 'wpb_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+}
+	
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+} // Class wpb_widget ends here
+
+// Register and load the widget
+function wpb_load_widget() {
+	register_widget( 'wpb_widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
+
+
+
+/*Theme options page*/
+if ( file_exists( STYLESHEETPATH . '/class.my-theme-options.php' ) ) {
+	require_once( STYLESHEETPATH . '/class.my-theme-options.php' );
+}
